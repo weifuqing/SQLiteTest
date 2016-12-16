7635f711f3c9afe94e68ab276a4f6a7aeb1f71fc
weifuqing <361251769@qq.com>
2016-12-16 18:26:27 +0800

    无聊的提交

--- app/src/main/java/com/example/personal/sqlitetest/entity/Person.java
+++ app/src/main/java/com/example/personal/sqlitetest/entity/Person.java
@@ -1,20 +1,53 @@
 package com.example.personal.sqlitetest.entity;
 
-import org.greenrobot.greendao.annotation.Entity;
-import org.greenrobot.greendao.annotation.Id;
 
 /**
  * Created by dell on 2016/12/12.
  */
 
-@Entity
 public class Person {
 
-    @Id
     private long _id;
     private int age;
     private String name;
-    private double grade;
-    private String address;
     private String hobby;
+
+    public Person(long _id, int age, String name, String hobby) {
+        this._id = _id;
+        this.age = age;
+        this.name = name;
+        this.hobby = hobby;
+    }
+
+    public long get_id() {
+        return _id;
+    }
+
+    public void set_id(long _id) {
+        this._id = _id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
 }
